2a50202db8cc660bec890cf208af730dd36d3305
CordzB <167384843+CordzB@users.noreply.github.com>
2024-10-17 00:41:11 -0600

    Update CalculosGeometricos.cs

--- EjercicioHerencia/CalculosGeometricos.cs
+++ EjercicioHerencia/CalculosGeometricos.cs
@@ -9,25 +9,28 @@ namespace EjercicioHerencia
             Console.WriteLine("-----------OPERACIONES-----------");
             Console.WriteLine();
             
-            double baseRect = 5.0, altura = 10.0;
+            double baseRect = 7.0, altura = 30.0;
             double areaRect = CalcularAreaRectangulo(baseRect, altura);
             Console.WriteLine($"Área del rectángulo: {areaRect}");
             Console.WriteLine();
 
-            double lado = 3.0;
+            double lado = 5.0;
             double volumenCubo = CalcularVolumenCubo(lado);
             Console.WriteLine($"Volumen del cubo: {volumenCubo}");
             Console.WriteLine();
 
-            double radio = 4.0;
+            double radio = 7.0;
             double longitudCirculo = CalcularLongitudCirculo(radio);
             Console.WriteLine($"Longitud del círculo: {longitudCirculo}");
             Console.WriteLine();
             
-            double num1 = 8.0; 
-            double num2 = 12.0; 
+            double num1 = 20.0; 
+            double num2 = 30.0; 
             double suma = Sumar(num1, num2);
             Console.WriteLine($"Suma de: {num1} y {num2}: {suma}");
+            Console.WriteLine();
+
+            Console.WriteLine($"-----MUCHAS GRACIAS-----");
         }
     }
 
